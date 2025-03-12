99ea35b8ce7d1c7f6f99ae488a450ae4e44ce6ea
Ramesh <ramesh@Mac.local>
2025-03-12 23:54:34 +0530

    Devcontainer setup done

---
+++ server/README.md
@@ -0,0 +1,2 @@
+## Start the container
+docker run -d --name arm_devcontainer -v "${PWD}:/workspace" arm-gcc-dev
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,6 +1,8 @@
 // main.cpp
 //
 
+#include <cstddef>
+#include <cstdio>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -10,9 +12,22 @@
 
 #include "tcp/include/runner.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
 
-  // Run task loop infinitely
-  run_task_loop();
+  // create a socket
+  int sockfd = socket(AF_PACKET, SOCK_RAW, 0x0003);
+  if (sockfd < 0) {
+    perror("Error in socker creation");
+    return 1;
+  }
+
+  printf("<----> Socket created <---->");
+  int result = getsockopt(sockfd, 1, 0, NULL, NULL);
+  if (result < 0) {
+    perror("Sockopt error");
+    return result;
+  }
+  printf("Socketoption = %d \n", result);
+
   return 0;
 }
